2248eb7ccce96773c5e7fabff2b668690bd5e12d
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-01-31 14:55:38 +0200

    added react app and angular package

---
+++ apps/webr/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div className={styles['background']}>
+      <h1>Welcome to my UI Kit</h1>
+      <p className={styles['red-letters']}>I am red!!!</p>
+    </div>
+  );
+}
+
+export default App;
